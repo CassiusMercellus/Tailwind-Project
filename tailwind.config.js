06d6140530fdb38f4dfd714d017956511673b45f
Cassius Hill <MarcelusWork@gmail.com>
2024-02-09 16:06:29 -0700

    finished project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*{html,js}",
-    "./public/index.html"
-  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      'xs': '0px',
+      // => @media (min-width: 0px) { ... }
 
+      'sm': '480px',
+      // => @media (min-width: 480px) { ... }
+
+      'md': '720px',
+      // => @media (min-width: 720px) { ... }
+
+      'lg': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'xl': '1199px',
+      // => @media (min-width: 1199px) { ... }
+    },
+  }
+}
